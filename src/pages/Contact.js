c75ca79a9eb4974a0d1a11d6fa1cfabb49bb9d8b
Jenny <jenny222@gmail.com>
2024-01-18 23:05:55 -0500

    Update files

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,9 +3,12 @@ import {Link} from "react-router-dom";
 
 const ContactPage=()=>{
     return (
+        <div className="contact-container">
+            <Link to="/" className="back-button">Back to HomePage</Link>
     <div className="contact">
         <h1>Email Address:</h1>
         
+    </div>
     </div>
     )
 }
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -3,9 +3,12 @@ import {Link} from "react-router-dom";
 
 const ProjectPage=()=>{
     return (
+        <div className="project-container">
+            <Link to="/" className="back-button">Back to HomePage</Link>
     <div className="project">
         <h1>This is project I created</h1>
         
+    </div>
     </div>
     )
 }
