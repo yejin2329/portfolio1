a0d73a66a52a7953451d547120aff193fa56423e
Jenny <jenny222@gmail.com>
2024-03-08 13:57:49 -0500

    Delete Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Routes, Route, Link} from "react-router-dom"
+import {Routes, Route} from "react-router-dom"
 import './App.css';
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
