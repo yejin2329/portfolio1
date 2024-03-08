fd74881d7304742621c0ca93927e77a64e5c52b4
Jenny <jenny222@gmail.com>
2024-03-08 04:32:44 -0500

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,8 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import phoneIcon from "../images/mobile.png";
-import emailIcon from "../images/email.png";
-import githubIcon from "../images/github.png";
+//import phoneIcon from "../images/mobile.png";
+//import emailIcon from "../images/email.png";
+//import githubIcon from "../images/github.png";
 
 const ContactPage=()=>{
     return (
@@ -10,17 +10,17 @@ const ContactPage=()=>{
             <Link to="/" className="back-button">Back to HomePage</Link>
     <div className="contact">
         <div className="email">
-        <img src={emailIcon} alt="email-address" className="email-address"/>
+
         Email Address: kimye232917@gmail.com
         </div>
         
         <div className="phone">
-        <img src={phoneIcon} alt="phonenumber" className="phonenumber"/>
+       
         2897727022
         </div>
 
         <div className="github">
-        <img src={githubIcon} alt="githuburl" className="githuburl"/>
+        
                 https://github.com/yejin2329/portfolio1.git</div>
     </div>
     </div>
