46d7fbd421606bdf7364898413ef87b256e386b6
yejin2329 <kimye232917@gmail.com>
2024-03-08 13:16:17 -0500

    Update test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+  <MemoryRouter>
+    <App />
+    </MemoryRouter>
+    );
+
+ // Check for the presence of elements you expect
+ const pageTitle = screen.getByText(/Welcome to the Future Web Developer's blog!/i);
+ const aboutMeLink = screen.getByText(/ABOUT ME/i);
+ const projectLink = screen.getByText(/PROJECT/i);
+ const contactLink = screen.getByText(/CONTACT/i);
+
+ // Assert that the elements are present
+ expect(pageTitle).toBeInTheDocument();
+ expect(aboutMeLink).toBeInTheDocument();
+ expect(projectLink).toBeInTheDocument();
+ expect(contactLink).toBeInTheDocument();
 });
