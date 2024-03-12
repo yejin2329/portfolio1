abbf19eb49709d623beb4a35f1481c3c356406b4
Jenny <jenny222@gmail.com>
2024-03-12 13:16:00 -0400

    Add images

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,8 @@
 import React from "react";
 import {Link} from "react-router-dom";
-//import phoneIcon from "../images/mobile.png";
-//import emailIcon from "../images/email.png";
-//import githubIcon from "../images/github.png";
+import phoneIcon from "../images/mobile.png";
+import emailIcon from "../images/email.png";
+import githubIcon from "../images/github.png";
 
 const ContactPage=()=>{
     return (
@@ -10,17 +10,19 @@ const ContactPage=()=>{
             <Link to="/" className="back-button">Back to HomePage</Link>
     <div className="contact">
         <div className="email">
-
+      
+        <img src={emailIcon} alt="email" className="email"/>
+              
         Email Address: kimye232917@gmail.com
         </div>
         
         <div className="phone">
-       
+        <img src={phoneIcon} alt="phone" className="phone"/>
         2897727022
         </div>
 
         <div className="github">
-        
+        <img src={githubIcon} alt="gitHub" className="gitHub"/>
                 https://github.com/yejin2329/portfolio1.git</div>
     </div>
     </div>
