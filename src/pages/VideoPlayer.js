b89d9c29df33fff0ef46c547363aec35a6a11acb
yejin2329 <kimye232917@gmail.com>
2024-04-17 23:45:51 -0400

    add video

--- src/pages/VideoPlayer.js
+++ src/pages/VideoPlayer.js
@@ -28,7 +28,22 @@ const VideoPlayer = () => {
         allowFullScreen
       ></iframe>
       </div>
+      <div className="update-text">
+        <h1>Update: April 16</h1>
+        </div>
+      <div className="video-container">
+      <iframe
+        title="Add feature Video"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/pBsvFSVmRnQ"
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      </div>
     </div>
+    
   );
 };
 
